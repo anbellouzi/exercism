ec52ad5c9ba02c9245c222b2dfb4412697122758
Anas <anbellouzi@gmail.com>
2020-04-27 13:08:58 -0700

    added problem solving strategies

--- high-scores/high_scores.py
+++ high-scores/high_scores.py
@@ -1,3 +1,31 @@
+'''
+High Schore
+-------
+Write methods that return the highest score from the list, the last added score and the three highest scores.
+
+You may assume: - scores is a list of unorderd integer values 
+                - There are no negative numbers
+
+RESTATE
+From a list of positive integers, return the highest num, last adderd num and the three highest nums in the list.
+
+ASSUMPTIONS
+We'll assume that input contains positive integers 0-n. 
+
+BRAINSTORM
+My initial idea is return the value at the last index, loop over scores and return the highest score, 
+then create three score variables, loop over score list and update scores accordinly. 
+
+EXPLAIN
+Last score: Here we have a 0(n) solution in which return the score at the last index.
+Highest score: Here we have  o(n^2) loop that checks if the score is greater than the high score. 
+
+TRADEOFFS
+Overall it's less memory efficient but is more time efficient.
+'''
+
+
+
 from heapq import nlargest
 
 
--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,8 +1,36 @@
-import enchant
+'''
+Word Count
+-------
+Given a phrase, count the occurrences of each word in that phrase.
+
+You may assume: - The count is case insensitive (ie "You", "you", and "YOU" are 3 uses of the same word)
+                - The count is unordered; the tests will ignore how words and counts are ordered
+                - Other than the apostrophe in a contraction all forms of punctuation are ignored
+                - The words can be separated by any form of whitespace (ie "\t", "\n", " ")
+
+RESTATE
+In a string or words and characters, return the count of all english words ignoring all other chars
+How long is the input list?
+
+ASSUMPTIONS
+We'll assume that input is a string containing words and special chars. 
+
+BRAINSTORM
+My initial idea is to loop over every char and saving it to another string called result.
+When I reach a special char, it will be replaced by a white space in the result string  
+
+EXPLAIN
+Here we have a 0(n^2) solution in which we check if each char is a alphabetical letter.
+Then we have another o(n^2) loop that checks if the word is already in hist,
+then simply add 1 to its count, or register it in hist with value 1. 
+
+TRADEOFFS
+Overall it's less time efficient but is more memory efficient.
+'''
 
 
 def count_words(sentence):
-    hist = {}
+    hist = {}                                           
     for c in sentence:
         if not c.isalnum():
             sentence = sentence.replace(c, " ")
